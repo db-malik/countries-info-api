6c0f7ba4f0b0f466ec0ae59b1654545e064db357
Malik <dbmalek92@gmail.com>
2022-11-14 21:14:15 +0100

    add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { ThemeContext } from '../../context/ThemeContext'
 
 const Header = () => {
+  const { theme, themeHandler } = useContext(ThemeContext)
+
   return (
-    <div>
+    <Container style={{ backgroundColor: theme.background, color: theme.text }}>
       <Title>Where in the world?</Title>
-      <Theme></Theme>
-    </div>
+      <Theme onClick={themeHandler}>
+        {theme.type === 'light' ? 'Dark mode' : 'Light mode'}
+      </Theme>
+    </Container>
   )
 }
 
+//  style begins
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+`
+
 const Title = styled.h1``
-const Theme = styled.p``
+const Theme = styled.p`
+  cursor: pointer;
+`
 
 export default Header
