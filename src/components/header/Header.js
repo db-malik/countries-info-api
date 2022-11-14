4727ec310bb38dc81a0b26ad95c908eba1d87a7d
Malik <dbmalek92@gmail.com>
2022-11-14 10:22:51 +0100

    add  header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <div>
+      <Title>Where in the world?</Title>
+      <Theme></Theme>
+    </div>
+  )
+}
+
+const Title = styled.h1``
+const Theme = styled.p``
+
+export default Header
