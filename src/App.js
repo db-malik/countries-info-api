6cb3e653127025bcab2d7e4cf2355855164c80fd
Malik <dbmalek92@gmail.com>
2022-11-14 21:04:40 +0100

    add route (react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import './App.css'
 import Header from './components/header/Header'
+
+import './App.css'
+import DetailPage from './pages/detail/DetailPage'
+import HomePage from './pages/home/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <Header></Header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="detail" element={<DetailPage />}></Route>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     )
   }
